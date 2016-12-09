742d559bf18007e09e5bdcbc53b9c56e4b0940a7
Kristina Lueth <kristina.lueth@publicispixelpark.de>
2016-12-09 14:22:13 +0100

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,41 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    postcss = require('gulp-postcss'),
+    cssnext  = require('postcss-cssnext'),
+    modernizr  = require('gulp-modernizr'),
+    watch  = require('gulp-watch');
 
 gulp.task('default', function(){
     console.log('test');
 });
 
-gulp.task('less', function() {
-    return gulp.src('less/styles.less')
-        .pipe(less())
-        .pipe(gulp.dest('css'))
+/* postcss */
+
+gulp.task('css', function() {
+    var plugins = [
+        cssnext
+    ];
+    return gulp.src('initial/*.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('final'));
+});
+
+/* modernizr */
+gulp.task('modernizr', function(){
+    return gulp.src('./js/*js')
+        .pipe(modernizr())
+        .pipe(gulp.dest("build/"));
+});
+
+/* watch */
+gulp.task('watch', function(){
+    return watch('css/**/*.css', { ignoreInitial: false})
+          .pipe(gulp.dest('build'));
+});
+
+gulp.task('callback', function () {
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('css/**/*.css', function () {
+        gulp.src('css/**/*.css')
+            .pipe(gulp.dest('build'));
+    });
 });
